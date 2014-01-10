8a16e7ddf6a55d9b456fbab1d349821643f88a65
Yaritza <saturn540@gmail.com>
2014-01-09 19:55:58 -0500

    change devise migration

--- app/models/mailbox.rb
+++ app/models/mailbox.rb
@@ -7,20 +7,10 @@ after_validation :geocode
 end
 
 
-# geocoded_by :address
-# after_validation :geocode, :if => :has_location, :unless => :has_coordinates
-
-# end
-
-# geocoded_by :full_street_address   # can also be an IP address
-# after_validation :geocode          # auto-fetch coordinates
-
-# end
-
 #   private
 
 #   def gmaps4rails_address
-#     "#{address}, #{self.city}, #{self.state}, #{self.zip}"
+#     "#{address}, #{self.longitude}, #{self.latitude}"
 #   end
 
 
