8f111ddacfae2f9d4ab7d36ee5a35e951459db73
Yaritza <saturn540@gmail.com>
2014-01-12 18:15:09 -0500

    rename app in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-MailboxEnroute::Application.configure do
+BlueboxFinder::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
